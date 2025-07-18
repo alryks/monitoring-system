e385337c5e5b9aa282c07f36ab498f182deb1c48
Nikita Sklyar <nikskl999@gmail.com>
2025-07-18 13:27:56 +0300

    Fixing errors

--- core/app/src/pages/Domains.tsx
+++ core/app/src/pages/Domains.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { api } from '../services/api';
-import styles from './domains.module.css';
+import styles from './Domains.module.css';
 
 interface Domain {
   id: string;
